5b1c002207419666804d1e8e5b26b68afbaadc19
timnajuch <14159338+timnajuch@users.noreply.github.com>
2026-02-24 00:01:26 +0100

    Parallelise output by replacing vtk with vti and pvti files.

--- src/fix_LBM_PSM_writeVTK.cpp
+++ src/fix_LBM_PSM_writeVTK.cpp
@@ -20,12 +20,28 @@ WriteVTK::WriteVTK(LAMMPS *lmp, int narg, char **arg) : Fix(lmp, narg, arg) {
     if(strcmp(modify->fix[ifix]->style,"lbm-psm")==0)
       fixLBMPSM = (fix_LBM_PSM *)modify->fix[ifix];
 
+  fileName = "flow_field";
+  binary_ = true;
+  useDouble_ = false;
+
   int iarg = 3;
   while (iarg < narg) {
     if (strcmp(arg[iarg],"every") == 0) {
       if (iarg+2 > narg) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
       nevery = atoi(arg[iarg+1]);
       if (nevery <= 0) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"binary") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
+      int readBinaryOption = atoi(arg[iarg+1]);
+      if (readBinaryOption < 0 || readBinaryOption > 1) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
+      binary_ = (bool)readBinaryOption;
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"useDouble") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
+      int readDoubleOption = atoi(arg[iarg+1]);
+      if (readDoubleOption < 0 || readDoubleOption > 1) error->all(FLERR,"Illegal fix lbm-psm-vtk command");
+      useDouble_ = (bool)readDoubleOption;
       iarg += 2;
     } else error->all(FLERR,"Illegal fix lbm-psm-vtk command");
   }
@@ -53,11 +69,7 @@ void WriteVTK::init()
   ny = fixLBMPSM->dynamics->get_ny();
   nz = fixLBMPSM->dynamics->get_nz();
 
-  ostringstream timeStringTmp;  
-  timeStringTmp << "flowField_" << setw(10) << setfill('0') << "0" << ".vtk";
-  string timeString(timeStringTmp.str());
-
-  write_vtk(timeString,
+  write_vtk_wrapper(fileName, 0, binary_, useDouble_,
             fixLBMPSM->dynamics->get_x_reference(), 1.0,
             fixLBMPSM->dynamics->get_y_reference(), 1.0,
             fixLBMPSM->dynamics->get_z_reference(), 1.0,
@@ -73,11 +85,7 @@ void WriteVTK::pre_force(int)
   double u_infty = fixLBMPSM->unitConversion->get_u_lb();
   double Uc = fixLBMPSM->unitConversion->get_Uc();
 
-  ostringstream timeStringTmp;
-  timeStringTmp << "flowField_" << setw(10) << setfill('0') << update->ntimestep << ".vtk";
-  string timeString(timeStringTmp.str());
-
-  write_vtk(timeString,
+  write_vtk_wrapper(fileName, update->ntimestep, binary_, useDouble_,
             fixLBMPSM->dynamics->get_x_reference(), 1.0,
             fixLBMPSM->dynamics->get_y_reference(), 1.0,
             fixLBMPSM->dynamics->get_z_reference(), 1.0,
@@ -85,238 +93,26 @@ void WriteVTK::pre_force(int)
             fixLBMPSM->dynamics->get_rho_reference(), fixLBMPSM->get_rho(),
             fixLBMPSM->dynamics->get_u_reference(), Uc/u_infty);
 
+    
 }
 
 
-void WriteVTK::write_vtk(string name_, vector<double> &x_, double x0_, vector<double> &y_, double y0_, vector<double> &z_, double z0_, vector<double> &B_, double B0_, vector<double> &rho_, double rho0_, vector<double> &u_, double u0_){
-  int envelopeWidth = 1;
-  int nzLoopStart = 0;
-  int nzLoopEnd = 1;
-  if(domain->dimension == 3){
-    nzLoopStart = envelopeWidth;
-    nzLoopEnd = nz-envelopeWidth;
-  }
 
-  vector<double> B_clip;
-  B_clip.resize(nx*ny*nz);
-  for (int i = 0; i < nx*ny*nz; ++i){
-    B_clip[i] = fixLBMPSM->dynamics->getParticleDataOnLatticeNode(i).solidFraction[0] + fixLBMPSM->dynamics->getParticleDataOnLatticeNode(i).solidFraction[1];
-    if (B_clip[i] > 1.0)
-      { B_clip[i] = 1.0; }
-  }
-
-  // Variables for MPI communication
-  int *MPIGathervCounts; // recvcounts definition / parameter in for MPI_Gatherv(...)
-  int *MPIGathervDispls; // displs definition for / parameter in MPI_Gatherv(...)
-  int *MPIGathervCounts3D; // recvcounts definition / parameter in for MPI_Gatherv(...)
-  int *MPIGathervDispls3D; // displs definition for / parameter in MPI_Gatherv(...)
- 
-  MPIGathervCounts = new int[comm->nprocs];
-  MPIGathervDispls = new int[comm->nprocs];
-  MPIGathervCounts3D = new int[comm->nprocs];
-  MPIGathervDispls3D = new int[comm->nprocs];
-
-  int nelements = nx*ny*nz;
-  int nelements3D = nx*ny*nz*3;
-  // Each process tells the root how many elements it holds
-  MPI_Gather(&nelements, 1, MPI_INT, MPIGathervCounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Gather(&nelements3D, 1, MPI_INT, MPIGathervCounts3D, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-  // Displacements in the receive buffer for MPI_GATHERV
-  for (int i = 0; i < comm->nprocs; i++){
-    MPIGathervDispls[i] = (i > 0) ? (MPIGathervDispls[i-1] + MPIGathervCounts[i-1]) : 0;
-    MPIGathervDispls3D[i] = (i > 0) ? (MPIGathervDispls3D[i-1] + MPIGathervCounts3D[i-1]) : 0;
-  }
-
-  vector<double> x0;
-  vector<double> y0;
-  vector<double> z0;
-  vector<double> B0;
-  vector<double> rho0;
-  vector<double> u0;
-  if (comm->me == 0){
-    x0.resize(MPIGathervDispls[comm->nprocs-1]+MPIGathervCounts[comm->nprocs-1]);
-    y0.resize(MPIGathervDispls[comm->nprocs-1]+MPIGathervCounts[comm->nprocs-1]);
-    z0.resize(MPIGathervDispls[comm->nprocs-1]+MPIGathervCounts[comm->nprocs-1]);
-    B0.resize(MPIGathervDispls[comm->nprocs-1]+MPIGathervCounts[comm->nprocs-1]);
-    rho0.resize(MPIGathervDispls[comm->nprocs-1]+MPIGathervCounts[comm->nprocs-1]);
-    u0.resize(MPIGathervDispls3D[comm->nprocs-1]+MPIGathervCounts3D[comm->nprocs-1]);
-  }
-
-  MPI_Gatherv(&x_[0], nelements, MPI_DOUBLE, &x0[0], MPIGathervCounts, MPIGathervDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(&y_[0], nelements, MPI_DOUBLE, &y0[0], MPIGathervCounts, MPIGathervDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(&z_[0], nelements, MPI_DOUBLE, &z0[0], MPIGathervCounts, MPIGathervDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(&B_clip[0], nelements, MPI_DOUBLE, &B0[0], MPIGathervCounts, MPIGathervDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(&rho_[0], nelements, MPI_DOUBLE, &rho0[0], MPIGathervCounts, MPIGathervDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(&u_[0], nelements3D, MPI_DOUBLE, &u0[0], MPIGathervCounts3D, MPIGathervDispls3D, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-  int nxTotal = 0;
-  for(int iproc = 0; iproc < decomposition[0]; iproc++){
-      nxTotal += fixLBMPSM->dynamics->get_nxLocal(iproc); }
-
-  int nyTotal = 0;
-    for(int jproc = 0; jproc < decomposition[1]; jproc++){
-      nyTotal += fixLBMPSM->dynamics->get_nyLocal(jproc); }
-
-  int nzTotal = 0;
-    for(int kproc = 0; kproc < decomposition[2]; kproc++){
-      nzTotal += fixLBMPSM->dynamics->get_nzLocal(kproc); }
-
-  if(comm->me == 0){
-    vector<double> x_vtk = x0;
-    scale_vector(x_vtk, x0_);
-
-    vector<double> y_vtk = y0;
-    scale_vector(y_vtk, y0_);
-
-    vector<double> z_vtk = z0;
-    scale_vector(z_vtk, z0_);
-
-    vector<double> B_vtk = B0;
-
-    vector<double> rho_vtk = rho0;
-    scale_vector(rho_vtk, rho0_);
-
-    vector<double> u_vtk = u0;
-    scale_vector(u_vtk, u0_);
-
-    ofstream ovel;
-    ovel.open(name_);
-    ovel << "# vtk DataFile Version 3.1 \n";
-    ovel << "Fluid flow field from LBM simulation\n";
-    ovel << "ASCII\n";
-    ovel << "DATASET RECTILINEAR_GRID\n";
-    if(domain->dimension == 2){
-      ovel << "DIMENSIONS " << nxTotal-2*envelopeWidth*decomposition[0] << " " << nyTotal-2*envelopeWidth*decomposition[1] << " " << 1 << "\n";
-    }else{
-      ovel << "DIMENSIONS " << nxTotal-2*envelopeWidth*decomposition[0] << " " << nyTotal-2*envelopeWidth*decomposition[1] << " " << nzTotal-2*envelopeWidth*decomposition[2] << "\n";
+void WriteVTK::write_vtk_wrapper(std::string name_, int timestep, bool binary, bool useDouble,
+                         vector<double> &x_, double x0_,
+                         vector<double> &y_, double y0_,
+                         vector<double> &z_, double z0_,
+                         std::vector<double> &rho_, double rho0_,
+                         std::vector<double> &u_, double u0_,
+                         std::vector<double> &B_, double B0_) 
+{
+    if (useDouble) {
+        execute_write_vtk<double>(name_, timestep, binary, x_, x0_,
+                         y_, y0_,
+                         z_, z0_,rho_, rho0_, u_, u0_, B_, B0_);
+    } else {
+        execute_write_vtk<float>(name_, timestep, binary, x_, x0_,
+                         y_, y0_,
+                         z_, z0_,rho_, rho0_, u_, u0_, B_, B0_);
     }
-
-    ovel << "X_COORDINATES " << nxTotal-2*envelopeWidth*decomposition[0] << " float\n";
-    for(int iproc = 0; iproc < decomposition[0]; iproc++){
-      for(int i = envelopeWidth; i < fixLBMPSM->dynamics->get_nxLocal(iproc)-envelopeWidth; i++){
-        int j = 0; int jproc = 0;
-        int k = 0; int kproc = 0;
-        int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-        int index = i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k + MPIGathervDispls[procIndex];
-        ovel << x_vtk[index] << "\n";
-      }
-    }
-
-    ovel << "Y_COORDINATES " << nyTotal-2*envelopeWidth*decomposition[1] << " float\n";
-    for(int jproc = 0; jproc < decomposition[1]; jproc++){
-      for(int j = envelopeWidth; j < fixLBMPSM->dynamics->get_nyLocal(jproc)-envelopeWidth; j++){
-        int i = 0; int iproc = 0;
-        int k = 0; int kproc = 0;
-        int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-        int index = i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k + MPIGathervDispls[procIndex];
-        ovel << y_vtk[index] << "\n";
-      }
-    }
-
-    if(domain->dimension == 2){
-      ovel << "Z_COORDINATES " << 1 << " float\n";
-      ovel << "0.0\n";
-    }else{
-      ovel << "Z_COORDINATES " << nzTotal-2*envelopeWidth*decomposition[2] << " float\n";
-      for(int kproc = 0; kproc < decomposition[2]; kproc++){
-        for(int k = envelopeWidth; k < fixLBMPSM->dynamics->get_nzLocal(kproc)-envelopeWidth; k++){
-          int i = 0; int iproc = 0;
-          int j = 0; int jproc = 0;
-          int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-          int index = i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k + MPIGathervDispls[procIndex];
-          ovel << z_vtk[index] << "\n";
-        }
-      }
-    }
-
-    // POINT_DATA
-    if(domain->dimension == 2){
-      ovel << "\nPOINT_DATA " << (nxTotal-envelopeWidth*2*decomposition[0])*(nyTotal-envelopeWidth*2*decomposition[1]) << "\n";
-    }else{
-      ovel << "\nPOINT_DATA " << (nxTotal-envelopeWidth*2*decomposition[0])*(nyTotal-envelopeWidth*2*decomposition[1])*(nzTotal-envelopeWidth*2*decomposition[2]) << "\n";
-    }
-    ovel << "SCALARS SolidFraction FLOAT\n";
-    ovel << "LOOKUP_TABLE default\n";
-    for(int kproc = 0; kproc<decomposition[2]; kproc++){
-      if(domain->dimension == 3){
-        nzLoopStart = envelopeWidth;
-        nzLoopEnd = fixLBMPSM->dynamics->get_nzLocal(kproc)-envelopeWidth;
-      }
-      for(int k=nzLoopStart; k<nzLoopEnd; k++){
-        for(int jproc = 0; jproc<decomposition[1]; jproc++){
-          for(int j=envelopeWidth; j<(fixLBMPSM->dynamics->get_nyLocal(jproc)-envelopeWidth); j++){
-            for(int iproc = 0; iproc<decomposition[0]; iproc++){
-              for(int i=envelopeWidth; i<(fixLBMPSM->dynamics->get_nxLocal(iproc)-envelopeWidth); i++){
-                int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-                int index = i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k + MPIGathervDispls[procIndex];
-                ovel << B_vtk[index] <<"\n";
-              }
-            }
-          }
-        }
-      }
-    }
-
-    ovel << "\nSCALARS Density FLOAT\n";
-    ovel << "LOOKUP_TABLE default\n";
-    for(int kproc = 0; kproc<decomposition[2]; kproc++){
-      if(domain->dimension == 3){
-        nzLoopStart = envelopeWidth;
-        nzLoopEnd = fixLBMPSM->dynamics->get_nzLocal(kproc)-envelopeWidth;
-      }
-      for(int k=nzLoopStart; k<nzLoopEnd; k++){
-        for(int jproc = 0; jproc<decomposition[1]; jproc++){
-          for(int j=envelopeWidth; j<(fixLBMPSM->dynamics->get_nyLocal(jproc)-envelopeWidth); j++){
-            for(int iproc = 0; iproc<decomposition[0]; iproc++){
-              for(int i=envelopeWidth; i<(fixLBMPSM->dynamics->get_nxLocal(iproc)-envelopeWidth); i++){
-                int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-                int index = i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k + MPIGathervDispls[procIndex];
-                ovel << rho_vtk[index] << "\n";
-              }
-            }
-          }
-        }
-      }
-    }
-
-    ovel << "\nVECTORS Velocity FLOAT\n";
-    for(int kproc = 0; kproc<decomposition[2]; kproc++){
-      if(domain->dimension == 3){
-        nzLoopStart = envelopeWidth;
-        nzLoopEnd = fixLBMPSM->dynamics->get_nzLocal(kproc)-envelopeWidth;
-      }
-      for(int k=nzLoopStart; k<nzLoopEnd; k++){
-        for(int jproc = 0; jproc<decomposition[1]; jproc++){
-          for(int j=envelopeWidth; j<(fixLBMPSM->dynamics->get_nyLocal(jproc)-envelopeWidth); j++){
-            for(int iproc = 0; iproc<decomposition[0]; iproc++){
-              for(int i=envelopeWidth; i<(fixLBMPSM->dynamics->get_nxLocal(iproc)-envelopeWidth); i++){
-                int procIndex = iproc*decomposition[1]*decomposition[2] + jproc*decomposition[2] + kproc;
-                int index = (i*fixLBMPSM->dynamics->get_nyLocal(jproc)*fixLBMPSM->dynamics->get_nzLocal(kproc) + j*fixLBMPSM->dynamics->get_nzLocal(kproc) + k)*3 + MPIGathervDispls3D[procIndex];
-                if(domain->dimension == 2){
-                  ovel << u_vtk[index] << " " << u_vtk[index+1] << " 0\n";
-                }else{
-                  ovel << u_vtk[index] << " " << u_vtk[index+1] << " " << u_vtk[index+2] << "\n";
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    ovel.close();
-  }
-
-  delete MPIGathervCounts;
-  delete MPIGathervDispls;
-  delete MPIGathervCounts3D;
-  delete MPIGathervDispls3D;
-};
-
-
-
-void WriteVTK::scale_vector(vector<double> &vec_, double scaling_){
-  std::transform(vec_.begin(), vec_.end(), vec_.begin(),
-               std::bind(std::multiplies<double>(), std::placeholders::_1, scaling_));
-};
+}
--- src/fix_LBM_PSM_writeVTK.h
+++ src/fix_LBM_PSM_writeVTK.h
@@ -28,6 +28,7 @@ FixStyle(lbm-psm-vtk,WriteVTK)
 #include <vector>
 #include <sstream>
 #include <string>
+#include <filesystem>
 
 #include "comm.h"
 #include "fix.h"
@@ -39,10 +40,15 @@ using namespace LAMMPS_NS;
 using namespace FixConst;
 using namespace std;
 
+namespace fs = std::filesystem;
+
 class WriteVTK : public Fix{
   private:
     int nx, ny, nz;
     int decomposition[3];
+    std::string fileName;
+    bool binary_;
+    bool useDouble_;
 
   public:
     WriteVTK(class LAMMPS *, int, char **);
@@ -51,13 +57,173 @@ class WriteVTK : public Fix{
     void init();
     void pre_force(int);
 
-  void write_vtk(string name_, vector<double> &x_, double x0_, vector<double> &y_, double y0_, vector<double> &z_, double z0_, vector<double> &B_, double B0_, vector<double> &rho_, double rho0_, vector<double> &u_, double u0_);
+    void write_vtk_wrapper(std::string name_, int timestep, bool binary, bool useDouble,
+                         vector<double> &x_, double x0_,
+                         vector<double> &y_, double y0_,
+                         vector<double> &z_, double z0_,
+                         vector<double> &B_, double B0_,
+                         vector<double> &rho_, double rho0_,
+                         vector<double> &u_, double u0_);
 
-  void scale_vector(vector<double> &vec_, double scaling_);
 
-  class fix_LBM_PSM *fixLBMPSM;
+    template <typename T> void execute_write_vtk(std::string name_, int timestep, bool binary,
+                         vector<double> &x_, double x0_,
+                         vector<double> &y_, double y0_,
+                         vector<double> &z_, double z0_,
+                         vector<double> &B_, double B0_,
+                         vector<double> &rho_, double rho0_,
+                         vector<double> &u_, double u0_);
+
+    class fix_LBM_PSM *fixLBMPSM;
 
 };
+
+
+template <typename T>
+void WriteVTK::execute_write_vtk(std::string name_, int timestep, bool binary,
+                                 vector<double> &x_, double x0_, vector<double> &y_, double y0_,
+                                 vector<double> &z_, double z0_, vector<double> &B_, double B0_,
+                                 vector<double> &rho_, double rho0_, vector<double> &u_, double u0_)
+{
+    int rank = comm->me;
+    int size = comm->nprocs;
+    int env = 1; 
+
+    std::string typeStr = (sizeof(T) == 8) ? "Float64" : "Float32";
+    size_t scalarSize   = sizeof(T);
+
+    // Create folder for output 
+    std::string folder = "flow_field_output_data";
+    if (rank == 0 && timestep == 0) {
+        if (fs::exists(folder)) fs::remove_all(folder);
+        fs::create_directory(folder);
+    }
+    MPI_Barrier(world);
+
+    // Indices and dimensions of lattice grid
+    std::vector<int> pC = fixLBMPSM->dynamics->get_procCoordinates();
+    int ip = pC[0], jp = pC[1], kp = pC[2];
+    int nxL = fixLBMPSM->dynamics->get_nxLocal(ip);
+    int nyL = fixLBMPSM->dynamics->get_nyLocal(jp);
+    int nzL = (domain->dimension == 3) ? fixLBMPSM->dynamics->get_nzLocal(kp) : 1;
+
+    int nx_i = nxL - 2 * env;
+    int ny_i = nyL - 2 * env;
+    int nz_i = (domain->dimension == 3) ? (nzL - 2 * env) : 1;
+
+    // Calculate the overlap/offset (there were problems in the output without)
+    int offsetX = 0; for (int i = 0; i < ip; i++) offsetX += (fixLBMPSM->dynamics->get_nxLocal(i) - 2 * env - 1);
+    int offsetY = 0; for (int j = 0; j < jp; j++) offsetY += (fixLBMPSM->dynamics->get_nyLocal(j) - 2 * env - 1);
+    int offsetZ = 0;
+    if (domain->dimension == 3) {
+        for (int k = 0; k < kp; k++) offsetZ += (fixLBMPSM->dynamics->get_nzLocal(k) - 2 * env - 1);
+    }
+
+    int nxTot = 0; for (int i = 0; i < decomposition[0]; i++) nxTot += (fixLBMPSM->dynamics->get_nxLocal(i) - 2 * env - 1); nxTot++;
+    int nyTot = 0; for (int j = 0; j < decomposition[1]; j++) nyTot += (fixLBMPSM->dynamics->get_nyLocal(j) - 2 * env - 1); nyTot++;
+    int nzTot = 1;
+    if (domain->dimension == 3) {
+        nzTot = 0; for (int k = 0; k < decomposition[2]; k++) nzTot += (fixLBMPSM->dynamics->get_nzLocal(k) - 2 * env - 1); nzTot++;
+    }
+
+    // Set the output path
+    std::ostringstream oss;
+    oss << name_ << "." << std::setw(8) << std::setfill('0') << timestep;
+    std::string baseFileName = oss.str();
+    std::string baseTimePath = folder + "/" + baseFileName;
+
+    // Clip the solid fraction at one and get also density + velocity for output (_o)
+    int nP = nx_i * ny_i * nz_i;
+    std::vector<T> B_o(nP), rho_o(nP), u_o(nP * 3);
+
+    int id = 0;
+    int zStart = (domain->dimension == 3) ? env : 0;
+    int zEnd   = (domain->dimension == 3) ? nzL - env : 1;
+
+    for (int k = zStart; k < zEnd; k++) {
+        for (int j = env; j < nyL - env; j++) {
+            for (int i = env; i < nxL - env; i++) {
+                int idx = i * nyL * nzL + j * nzL + k;
+                auto pD = fixLBMPSM->dynamics->getParticleDataOnLatticeNode(idx);
+                double Bv = (pD.solidFraction[0] + pD.solidFraction[1]);
+                
+                // Casting for T (float or double) // if double not needed (e.g. for detailed postprocessing), can save space
+                B_o[id]     = static_cast<T>((Bv > 1.0 ? 1.0 : Bv) * B0_);
+                rho_o[id]   = static_cast<T>(rho_[idx] * rho0_);
+                u_o[3*id+0] = static_cast<T>(u_[3*idx+0] * u0_);
+                u_o[3*id+1] = static_cast<T>(u_[3*idx+1] * u0_);
+                u_o[3*id+2] = (domain->dimension == 3) ? static_cast<T>(u_[3*idx+2] * u0_) : static_cast<T>(0.0);
+                id++;
+            }
+        }
+    }
+
+    // Write the local vti files. One file per core
+    std::string vfname = baseTimePath + "_rank" + std::to_string(rank) + ".vti";
+    int xE = offsetX + nx_i - 1;
+    int yE = offsetY + ny_i - 1;
+    int zE_ext = (domain->dimension == 3) ? (offsetZ + nz_i - 1) : 0;
+
+    if (binary) {
+        std::ofstream vti(vfname, std::ios::out | std::ios::binary);
+        uint32_t b_sz = (uint32_t)(nP * scalarSize);
+        uint32_t u_sz = (uint32_t)(nP * 3 * scalarSize);
+
+        vti << "<?xml version=\"1.0\"?>\n<VTKFile type=\"ImageData\" version=\"1.0\" byte_order=\"LittleEndian\" header_type=\"UInt32\">\n";
+        vti << " <ImageData WholeExtent=\"0 " << nxTot-1 << " 0 " << nyTot-1 << " 0 " << nzTot-1 << "\" Origin=\"0 0 0\" Spacing=\"1 1 1\">\n";
+        vti << "  <Piece Extent=\"" << offsetX << " " << xE << " " << offsetY << " " << yE << " " << offsetZ << " " << zE_ext << "\">\n";
+        vti << "   <PointData>\n";
+        vti << "    <DataArray type=\"" << typeStr << "\" Name=\"SolidFraction\" format=\"appended\" offset=\"0\"/>\n";
+        vti << "    <DataArray type=\"" << typeStr << "\" Name=\"Density\" format=\"appended\" offset=\"" << (4 + b_sz) << "\"/>\n";
+        vti << "    <DataArray type=\"" << typeStr << "\" Name=\"Velocity\" NumberOfComponents=\"3\" format=\"appended\" offset=\"" << (2*(4 + b_sz)) << "\"/>\n";
+        vti << "   </PointData>\n  </Piece>\n </ImageData>\n <AppendedData encoding=\"raw\">_";
+        vti.flush();
+
+        vti.write((char*)&b_sz, 4); vti.write((char*)B_o.data(), b_sz);
+        vti.write((char*)&b_sz, 4); vti.write((char*)rho_o.data(), b_sz);
+        vti.write((char*)&u_sz, 4); vti.write((char*)u_o.data(), u_sz);
+        vti << "\n </AppendedData>\n</VTKFile>";
+    } else {
+        std::ofstream vti(vfname);
+        vti << "<?xml version=\"1.0\"?>\n<VTKFile type=\"ImageData\" version=\"0.1\">\n";
+        vti << " <ImageData WholeExtent=\"0 " << nxTot-1 << " 0 " << nyTot-1 << " 0 " << nzTot-1 << "\">\n";
+        vti << "  <Piece Extent=\"" << offsetX << " " << xE << " " << offsetY << " " << yE << " " << offsetZ << " " << zE_ext << "\">\n";
+        vti << "   <PointData>\n";
+        
+        auto ascii = [&](std::string nm, int comp, const std::vector<T>& data) {
+            vti << "    <DataArray type=\"" << typeStr << "\" Name=\"" << nm << "\" NumberOfComponents=\"" << comp << "\" format=\"ascii\">\n";
+            for(size_t i=0; i<data.size(); i++) vti << data[i] << (i%10==9 ? "\n" : " ");
+            vti << "\n    </DataArray>\n";
+        };
+        ascii("SolidFraction", 1, B_o);
+        ascii("Density", 1, rho_o);
+        ascii("Velocity", 3, u_o);
+        vti << "   </PointData>\n  </Piece>\n </ImageData>\n</VTKFile>";
+    }
+
+    // Write the master pvti file on core 0
+    int lEx[6] = {offsetX, xE, offsetY, yE, offsetZ, zE_ext};
+    std::vector<int> allEx;
+    if (rank == 0) allEx.resize(6 * size);
+    MPI_Gather(lEx, 6, MPI_INT, allEx.data(), 6, MPI_INT, 0, world);
+
+    if (rank == 0) {
+        std::ofstream pvti(baseTimePath + ".pvti");
+        pvti << "<?xml version=\"1.0\"?>\n<VTKFile type=\"PImageData\" version=\"0.1\">\n";
+        pvti << " <PImageData WholeExtent=\"0 " << nxTot-1 << " 0 " << nyTot-1 << " 0 " << nzTot-1 << "\" Origin=\"0 0 0\" Spacing=\"1 1 1\">\n";
+        pvti << "  <PPointData>\n";
+        pvti << "   <PDataArray type=\"" << typeStr << "\" Name=\"SolidFraction\"/>\n";
+        pvti << "   <PDataArray type=\"" << typeStr << "\" Name=\"Density\"/>\n";
+        pvti << "   <PDataArray type=\"" << typeStr << "\" Name=\"Velocity\" NumberOfComponents=\"3\"/>\n";
+        pvti << "  </PPointData>\n";
+        for (int i = 0; i < size; i++) {
+            int* e = &allEx[i*6];
+            pvti << "  <Piece Extent=\"" << e[0] << " " << e[1] << " " << e[2] << " " << e[3] << " " << e[4] << " " << e[5] 
+                 << "\" Source=\"" << baseFileName << "_rank" << i << ".vti\"/>\n";
+        }
+        pvti << " </PImageData>\n</VTKFile>";
+    }
+}
 
 #endif
 #endif
